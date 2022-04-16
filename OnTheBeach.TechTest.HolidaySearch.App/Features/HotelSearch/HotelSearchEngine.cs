4505209b988fe865d7834fccf6aabc1990147f18
Dan Ashurst <dan@danashurst.com>
2022-04-16 17:49:35 +0100

    test: write test case for hotel search for customer 1

---
+++ OnTheBeach.TechTest.HolidaySearch.App/Features/HotelSearch/HotelSearchEngine.cs
@@ -0,0 +1,16 @@
+﻿using OnTheBeach.TechTest.HolidaySearch.App.Models;
+
+namespace OnTheBeach.TechTest.HolidaySearch.App.Features.HotelSearch;
+
+public interface IHotelSearchEngine
+{
+    IEnumerable<Hotel> SearchHotels(IEnumerable<Hotel> availableHotels, HolidayRequirements holidayRequirements);
+}
+
+public class HotelSearchEngine : IHotelSearchEngine
+{
+    public IEnumerable<Hotel> SearchHotels(IEnumerable<Hotel> availableHotels, HolidayRequirements holidayRequirements)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ OnTheBeach.TechTest.HolidaySearch.Tests/HotelSearchTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using OnTheBeach.TechTest.HolidaySearch.App;
+using OnTheBeach.TechTest.HolidaySearch.App.Features.HotelSearch;
+using OnTheBeach.TechTest.HolidaySearch.App.Models;
+using Shouldly;
+using Xunit;
+
+namespace OnTheBeach.TechTest.HolidaySearch.Tests;
+
+public class HotelSearchTests
+{
+    private readonly IDataLoader _dataLoader;
+    private readonly IHotelSearchEngine _hotelSearchEngine;
+    
+    public HotelSearchTests()
+    {
+        _dataLoader = new DataLoader();
+        _hotelSearchEngine = new HotelSearchEngine();
+    }
+    
+    [Fact]
+    public void ShouldMatchCorrectHotelForCustomer1()
+    {
+        // Arrange
+        var availableHotels = _dataLoader.GetHotels();
+        
+        // Act
+        var holidayRequirements = new HolidayRequirements
+        {
+            DepartureAirport = "MAN",
+            DestinationAirport = "APG",
+            DepartureDate = new DateTime(2023, 7, 1),
+            Duration = 7
+        };
+
+        var matchedHotels = _hotelSearchEngine.SearchHotels(availableHotels, holidayRequirements);
+
+        // Assert
+        matchedHotels.First().Id.ShouldBe(9);
+
+    }
+}
